fd7b21e00fcefc071f4877ec5f153e67d6287ef7
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2022-12-29 17:43:48 -0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Fernando Moraes on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
